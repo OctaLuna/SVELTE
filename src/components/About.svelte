676d8bd3b6b433fcba9e557bcd7561d22186ca3b
Marco Octavio luna Vargas <marco.octavio.luna@gmail.com>
2021-06-24 17:40:54 -0400

    Commit 3; Se programo el script About, se uso la Reactividad, se creo un contador de click

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -2,6 +2,14 @@
 <script>
     //Estamos creando un variable donde tenemos escrito nuestra descripción
     let someText = "Data Science Developer at Platzi";
+    //Esta variable es para crear un contador
+    let count = 0;
+
+    //Esto es para manejar el contador de los click del boton
+    function handleClick(){
+        count += 1;
+    }
+
 </script>
 
 <style>
@@ -16,4 +24,8 @@
     <p>
         {someText}
     </p>
+    <!--Estamos creando un boton para nuestro contador, {count} estamos referenciando la variable count que es el contador
+    on:click={handleClick} Esto es para que cada vez que le demos click a nuestro boton llamamos a la funcion handleClick
+    count == 0 ? "" : count Esto es para que, cuando el valor de count es 0 no muestre nada pero si no es 0 muestre el contador-->
+    <button on:click={handleClick}>Click {count == 0 ? "" : count}</button>
 </div>
